569263e722b1147314b20f3a73db530163321ac4
Andrew Knight <andrewjaknight@gmail.com>
2017-01-12 10:48:40 +1100

    First commit - adding a simple node blog post

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node blog post
+
+## A simple Node blog post application using Node, Express, MongoDB and bootstrap.
---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    Post = require('./Post');
+
+var app = express();
+
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
+
+// Setting up authorisation
+var auth = express.basicAuth(function(username, password) {
+    return username === 'foo' && password === 'bar';
+});
+
+app.use(auth);
+
+// Render home page with all blog posts
+app.get('/', function(request, response) {
+    Post.find(function(err, posts) {
+        if (err) {
+            response.send(500, 'There was an error - tough luck.');
+        }
+        else {
+            response.render('index', {
+                posts:posts
+            });
+        }
+    });
+});
+
+// Setting up a json response
+app.get('/posts.json', function(request, response) {
+    Post.find(function(err, posts) {
+        if (err) {
+            response.send(500, {
+                success:false
+            });
+        }
+        else {
+            response.send({
+                success:true,
+                posts:posts
+            });
+        }
+    });
+});
+
+// Render a form to enter a new post
+app.get('/new', auth, function(request, response) {
+    response.render('new', {});
+});
+
+// Create a new blog post object
+app.post('/create', auth, function(request, response) {
+    //Create and save a Post model
+    var post = new Post({
+        title: request.body.title,
+        content: request.body.content
+    });
+
+    //Save the model
+    post.save(function(err, model) {
+        if (err) {
+            response.send(500, 'There was an error - tough luck.');
+        }
+        else {
+            response.redirect('/');
+        }
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});
